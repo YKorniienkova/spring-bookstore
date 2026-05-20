310e394db6ff77b324a99bcfbef652291b4d21ca
Yelyzaveta Korniienkova <yelyzavetakorniienkova@gmail.com>
2026-05-21 01:28:10 +0200

    Implement book entity

--- src/main/java/mate/academy/springintro/SpringIntroApplication.java
+++ src/main/java/mate/academy/springintro/SpringIntroApplication.java
@@ -1,13 +1,38 @@
 package mate.academy.springintro;
 
+import java.math.BigDecimal;
+import mate.academy.springintro.model.Book;
+import mate.academy.springintro.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringIntroApplication {
+    @Autowired
+    private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringIntroApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(){
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                Book book1 = new Book();
+                book1.setAuthor("Taras");
+                book1.setTitle("Book");
+                book1.setIsbn("978-3-16-148410-0");
+                book1.setPrice(BigDecimal.valueOf(200));
+                book1.setDescription("It's a book");
+                book1.setCoverImage("image");
+                bookService.save(book1);
+                System.out.println(bookService.findAll());
+            }
+        };
+    }
 }
---
+++ src/main/java/mate/academy/springintro/service/impl/BookServiceImpl.java
@@ -0,0 +1,28 @@
+package mate.academy.springintro.service.impl;
+
+import java.util.List;
+import mate.academy.springintro.model.Book;
+import mate.academy.springintro.repository.BookRepository;
+import mate.academy.springintro.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
