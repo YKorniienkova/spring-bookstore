89b229fcbba14dc68dfda9cc893521bb731a8a09
Yelyzaveta Korniienkova <yelyzavetakorniienkova@gmail.com>
2026-06-11 11:23:39 +0200

    Add category entity and repository

--- src/main/java/mate/academy/springintro/model/Book.java
+++ src/main/java/mate/academy/springintro/model/Book.java
@@ -1,12 +1,11 @@
 package mate.academy.springintro.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
@@ -37,6 +36,13 @@ public class Book {
 
     private String description;
     private String coverImage;
+    @ManyToMany
+    @JoinTable(
+            name = "books_categories",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories = new HashSet<>();
 
     @Column(nullable = false)
     private boolean isDeleted = false;
