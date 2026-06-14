43542351b5bae2012b2bc7725559b969c08dfd70
Yelyzaveta Korniienkova <yelyzavetakorniienkova@gmail.com>
2026-06-14 11:42:22 +0200

    Fix mentor comments

--- src/main/java/mate/academy/springintro/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/springintro/service/impl/AuthenticationServiceImpl.java
@@ -1,18 +1,13 @@
-package mate.academy.springintro.security;
+package mate.academy.springintro.service.impl;
 
-import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import mate.academy.springintro.dto.user.UserLoginRequestDto;
 import mate.academy.springintro.dto.user.UserLoginResponseDto;
-import mate.academy.springintro.dto.user.UserRegistrationRequestDto;
-import mate.academy.springintro.dto.user.UserResponseDto;
-import mate.academy.springintro.exception.EntityNotFoundException;
-import mate.academy.springintro.exception.RegistrationException;
 import mate.academy.springintro.mapper.UserMapper;
-import mate.academy.springintro.model.Role;
-import mate.academy.springintro.model.User;
 import mate.academy.springintro.repository.RoleRepository;
 import mate.academy.springintro.repository.UserRepository;
+import mate.academy.springintro.security.JwtUtil;
+import mate.academy.springintro.service.AuthenticationService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -29,23 +24,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private final RoleRepository roleRepository;
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
-
-    @Override
-    public UserResponseDto register(UserRegistrationRequestDto requestDto)
-            throws RegistrationException {
-        if (userRepository.existsByEmail(requestDto.getEmail())) {
-            throw new RegistrationException("User with email "
-                    + requestDto.getEmail() + " already exists");
-        }
-        Role userRole = roleRepository.findByName(Role.RoleName.ROLE_USER)
-                .orElseThrow(() -> new EntityNotFoundException("Role "
-                        + Role.RoleName.ROLE_USER + " not found"));
-        User user = userMapper.toModel(requestDto);
-        user.setPassword(passwordEncoder.encode(requestDto.getPassword()));
-        user.setRoles(Set.of(userRole));
-        User savedUser = userRepository.save(user);
-        return userMapper.toDto(savedUser);
-    }
 
     @Override
     public UserLoginResponseDto authenticate(UserLoginRequestDto requestDto) {
