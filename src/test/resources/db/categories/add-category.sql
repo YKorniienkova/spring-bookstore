c194eb50cbdc664634652f26a48dd5e936e7c596
Yelyzaveta Korniienkova <yelyzavetakorniienkova@gmail.com>
2026-06-29 10:16:19 +0200

    Fix category and book test cleanup after adding seed data

--- src/test/resources/db/categories/add-category.sql
+++ src/test/resources/db/categories/add-category.sql
@@ -1,2 +1,5 @@
+DELETE FROM books_categories;
+DELETE FROM categories;
+
 INSERT INTO categories (id, name, description, is_deleted)
 VALUES (1, 'Programming', 'Programming books', 0);
--- src/test/resources/db/categories/add-two-categories.sql
+++ src/test/resources/db/categories/add-two-categories.sql
@@ -1,3 +1,6 @@
+DELETE FROM books_categories;
+DELETE FROM categories;
+
 INSERT INTO categories (id, name, description, is_deleted)
 VALUES
 (1, 'Programming', 'Programming books', 0),
